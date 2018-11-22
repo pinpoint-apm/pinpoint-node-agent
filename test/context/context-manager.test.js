e8bde13c03217e1b34117f5dbecc0d26374590c5
j.h.lee <jundol.lee@navercorp.com>
2018-11-23 00:42:45 +0900

    Fix tracing async context

---
+++ test/context/context-manager.test.js
@@ -0,0 +1,67 @@
+const test = require('tape')
+
+const contextManger = require('context/context-manager')
+
+test('Should trace context in setTimeout', async function (t) {
+  t.plan(1)
+
+  const initVal = 'value-for-test'
+  contextManger.setObject(initVal)
+
+  setTimeout(() => {
+    const currentVal = contextManger.getObject()
+    t.equal(currentVal, initVal)
+  }, 300)
+})
+
+test('Should trace context in setInterval', async function (t) {
+  t.plan(1)
+
+  const initVal = 'value-for-test'
+  contextManger.setObject(initVal)
+
+  const interval = setInterval(() => {
+    const currentVal = contextManger.getObject()
+    t.equal(currentVal, initVal)
+    clearInterval(interval)
+  }, 300)
+})
+
+
+test('Should trace context in Promise', function (t) {
+  t.plan(2)
+
+  const initVal = 'value-for-test'
+  contextManger.setObject(initVal)
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const currentVal = contextManger.getObject()
+      t.equal(currentVal, initVal)
+      resolve()
+    }, 300)
+  })
+
+  promise.then(() => {
+    const resolvedVal = contextManger.getObject()
+    t.equal(resolvedVal, initVal)
+  })
+})
+
+test('Should trace context in setTimeout multiple times', async function (t) {
+  t.plan(2)
+
+  let initVal = 1
+  contextManger.setObject(initVal)
+
+  setTimeout(() => {
+    const currentVal = contextManger.getObject()
+    t.equal(currentVal, initVal)
+    contextManger.setObject(++initVal)
+
+    setTimeout(() => {
+      const currentVal = contextManger.getObject()
+      t.equal(currentVal, initVal)
+    }, 300)
+  }, 300)
+})
--- test/context/trace.test.js
+++ test/context/trace.test.js
@@ -14,7 +14,7 @@ test('Should begin/end trace block', async function (t) {
   t.equal(trace.sequence, 1)
 
   await util.sleep(11)
-  trace.traceBlockEnd()
+  trace.traceBlockEnd(spanEventRecorder)
 
   console.log('spanEventRecorder.spanEvent.elapsedTime', spanEventRecorder.spanEvent.elapsedTime)
   t.ok(spanEventRecorder.spanEvent.elapsedTime > 10)
@@ -25,11 +25,11 @@ test('Should push/pop call stack ', function (t) {
 
   const traceId = fixture.getTraceId()
   const trace = new Trace(traceId)
-  trace.traceBlockBegin()
-  const spanEventRecorder = trace.traceBlockBegin()
-  t.equal(trace.callStack.length, spanEventRecorder.spanEvent.depth)
+  const spanEventRecorder1 = trace.traceBlockBegin()
+  const spanEventRecorder2 = trace.traceBlockBegin()
+  t.equal(trace.callStack.length, 2)
 
-  trace.traceBlockEnd()
-  trace.traceBlockEnd()
+  trace.traceBlockEnd(spanEventRecorder2)
+  trace.traceBlockEnd(spanEventRecorder1)
   t.equal(trace.callStack.length, 0)
 })

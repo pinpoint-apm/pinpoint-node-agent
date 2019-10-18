62690f1fd174562119019d18b1ddb60998313d64
yongseok kang <yongseok.kang@navercorp.com>
2019-10-18 12:04:43 +0900

    String env unit test #84

--- test/agent/env-config.test.js
+++ test/agent/env-config.test.js
@@ -1,4 +1,13 @@
 const test = require('tape')
 const config = require('../../lib/config')
 
-
+test('should return the agentId string value when the env value is string type', function(t) {
+    process.env['PINPOINT_AGENT_ID'] = "agentId"
+    process.env['PINPOINT_APPLICATION_NAME'] = "appication name"
+    process.env['PINPOINT_COLLECTOR_IP'] = "***REMOVED***"
+    const given = config.getConfig()
+    t.equal(given.agentId, "agentId", "given PINPOINT_AGENT_ID env, should equal config")
+    t.equal(given.applicationName, "appication name", "given PINPOINT_APPLICATION_NAME env, should equal config")
+    t.equal(given.collectorIp, "***REMOVED***", "given PINPOINT_COLLECTOR_IP env, should equal config")
+    t.end()
+});

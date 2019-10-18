cca600284d550023b43d1789b1a1ca05138f69be
yongseok kang <yongseok.kang@navercorp.com>
2019-10-18 12:40:30 +0900

    String env unit test #84

--- test/agent/env-config.test.js
+++ test/agent/env-config.test.js
@@ -18,26 +18,30 @@ test('should return the string value when the env value is string type', functio
     delete process.env.PINPOINT_COLLECTOR_IP
     delete process.env.PINPOINT_LOG_LEVEL
 
+    config.clear()
     t.end()
 })
 
 test('should return the number value when the env value is number type', function(t) {
     process.env['PINPOINT_SERVICE_TYPE'] = "1400"
-    process.env['PINPOINT_COLLECTOR_TCP_PORT'] = "1400"
-    process.env['PINPOINT_COLLECTOR_STAT_PORT'] = "1400"
-    process.env['PINPOINT_COLLECTOR_SPAN_PORT'] = "1400"
-    // process.env['PINPOINT_SAMPLING_RATE'] = "10"
+    process.env['PINPOINT_COLLECTOR_TCP_PORT'] = "9894"
+    process.env['PINPOINT_COLLECTOR_STAT_PORT'] = "9895"
+    process.env['PINPOINT_COLLECTOR_SPAN_PORT'] = "9896"
+    process.env['PINPOINT_SAMPLING_RATE'] = "10"
 
     const given = config.getConfig()
     t.equal(given.serviceType, 1400, 'given PINPOINT_SERVICE_TYPE env, should equal config')
-    t.equal(given.collectorTcpPort, 1400, 'given PINPOINT_COLLECTOR_TCP_PORT env, should equal config')
-    t.equal(given.collectorStatPort, 1400, 'given PINPOINT_COLLECTOR_STAT_PORT env, should equal config')
-    t.equal(given.collectorSpanPort, 1400, 'given PINPOINT_COLLECTOR_SPAN_PORT env, should equal config')
-    // t.equal(given.sampleRate, 10, 'given PINPOINT_SAMPLING_RATE env, should equal config')
+    t.equal(given.collectorTcpPort, 9894, 'given PINPOINT_COLLECTOR_TCP_PORT env, should equal config')
+    t.equal(given.collectorStatPort, 9895, 'given PINPOINT_COLLECTOR_STAT_PORT env, should equal config')
+    t.equal(given.collectorSpanPort, 9896, 'given PINPOINT_COLLECTOR_SPAN_PORT env, should equal config')
+    t.equal(given.sampleRate, 10, 'given PINPOINT_SAMPLING_RATE env, should equal config')
 
     delete process.env.PINPOINT_SERVICE_TYPE
     delete process.env.PINPOINT_COLLECTOR_TCP_PORT
     delete process.env.PINPOINT_COLLECTOR_STAT_PORT
     delete process.env.PINPOINT_COLLECTOR_SPAN_PORT
+    delete process.env.PINPOINT_SAMPLING_RATE
+
+    config.clear()
     t.end()
 })

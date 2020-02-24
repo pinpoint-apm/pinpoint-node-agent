008636b4b3d231e61d96653f4f08d76987937f84
yongseok kang <yongseok.kang@navercorp.com>
2020-02-24 17:16:02 +0900

    echoTest server test common code

--- test/agent/outgoing.test.js
+++ test/agent/outgoing.test.js
@@ -1,20 +1,21 @@
 const test = require('tape')
+const http = require('http')
 const echoServer = require('./support/echo-server-instance')
 
-test('http.request(options)', echoTest('http', (port, cb) => {
-    const options = { port }
-    const req = http.request(options)
-    req.on('response', cb)
-    return req
-}))
+// test('http.request(options)', echoTest('http', (port, cb) => {
+//     const options = { port }
+//     const req = http.request(options)
+//     req.on('response', cb)
+//     return req
+// }))
 
 function echoTest(protocol, handler) {
     return function(t) {
         echoServer(protocol, (cp, port) => {
             console.log(`cp ${cp}, port ${port}`)
-            t.end()
-            cp.kill()
-            
+            // t.end()
+            // cp.kill()
+
             const req = handler(port, res => {
                 res.on('end', () => {
                     

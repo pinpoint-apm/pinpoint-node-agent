12b64b76b24736ad59d93955d2311fdfccb08755
yongseok kang <yongseok.kang@navercorp.com>
2020-02-24 16:46:57 +0900

    Tape configure for vscode

--- test/agent/outgoing.test.js
+++ test/agent/outgoing.test.js
@@ -1,8 +1,26 @@
 const test = require('tape')
-const config = require('../../lib/config')
+const echoServer = require('./support/echo-server-instance')
 
-test('load a https module', (t) => {
-    
-    
-    t.end()
-})
+test('http.request(options)', echoTest('http', (port, cb) => {
+    const options = { port }
+    const req = http.request(options)
+    req.on('response', cb)
+    return req
+}))
+
+function echoTest(protocol, handler) {
+    return function(t) {
+        echoServer(protocol, (cp, port) => {
+            console.log(`cp ${cp}, port ${port}`)
+            t.end()
+            cp.kill()
+            
+            const req = handler(port, res => {
+                res.on('end', () => {
+                    
+                })
+                res.resume()
+            })
+        })
+    }
+}
--- test/agent/support/echo-server.js
+++ test/agent/support/echo-server.js
@@ -2,15 +2,17 @@
 
 const http = require('http')
 
+process.title = 'echo-server'
+
 const server = http.createServer()
 server.on("request", (req, res) => {
     req.pipe(res)
 })
 
 server.listen(() => {
-    
+    console.log(server.address().port)
 })
 
 setTimeout(() => {
     server.close()
-}, 60 * 1000)
+}, 6 * 1000)

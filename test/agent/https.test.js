96bcd4a489c1807d702d9c5e8b418fe2ed618765
yongseok kang <yongseok.kang@navercorp.com>
2020-02-24 13:45:21 +0900

    echo server

--- test/agent/https.test.js
+++ test/agent/https.test.js
@@ -2,7 +2,7 @@ const test = require('tape')
 const config = require('../../lib/config')
 
 test('load a https module', (t) => {
-
+    
     
     t.end()
 })
--- test/agent/support/echo-server-instance.js
+++ test/agent/support/echo-server-instance.js
@@ -3,6 +3,17 @@
 const exec = require('child_process').exec
 const path = require('path')
 
-module.exports = (type, cb) => {
+module.exports = echoServer
 
+function echoServer(protocol, callback) {
+    if (typeof protocol == 'function')
+        return echoServer('http', protocol)
+
+    const echoServerScript = path.join(__dirname, 'echo-server.js')
+    const callbackProcess = exec(`node "${echoServerScript}" ${protocol}`)
+    callbackProcess.stderr.pipe(process.stderr)
+    callbackProcess.stdout.once('data', (chunk) => {
+        const port = chunk.trim().split('\n')[0]
+        callback(callbackProcess, port)
+    })
 }

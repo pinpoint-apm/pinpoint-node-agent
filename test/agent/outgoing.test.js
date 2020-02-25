29de78b78aa1897ead185ff43e4779ff0fef0e00
yongseok kang <yongseok.kang@navercorp.com>
2020-02-25 12:09:34 +0900

    response test

--- test/agent/outgoing.test.js
+++ test/agent/outgoing.test.js
@@ -2,12 +2,12 @@ const test = require('tape')
 const http = require('http')
 const echoServer = require('./support/echo-server-instance')
 
-// test('http.request(options)', echoTest('http', (port, cb) => {
-//     const options = { port }
-//     const req = http.request(options)
-//     req.on('response', cb)
-//     return req
-// }))
+test('http.request(options)', echoTest('http', (port, cb) => {
+    const options = { port }
+    const req = http.request(options)
+    req.on('response', cb)
+    return req
+}))
 
 function echoTest(protocol, handler) {
     return function(t) {

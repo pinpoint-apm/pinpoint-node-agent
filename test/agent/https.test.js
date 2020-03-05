b322f6e4d651568612af5e9b1df87517fc2a5ecd
Yongseok Kang <yongseok.kang@navercorp.com>
2020-03-05 21:43:27 +0900

    disable integration test for unit test

--- test/agent/https.test.js
+++ test/agent/https.test.js
@@ -1,2 +1,27 @@
 'use strict'
 
+const test = require('tape')
+const rewire = require('rewire')
+const instManager = rewire('../../lib/instrumentation/inst-manager')
+
+test('initialize https module', (t) => {
+    const agent = {
+        modules: [],
+        includedModules: function(name) {
+            this.modules.push(name)
+        },
+        setModules: function(name) {
+
+        }
+    }
+
+    const dut = instManager.init(agent)
+    
+    var loadModule = instManager.__get__('loadModule');
+    loadModule('https', agent)
+    loadModule('kmkm', agent)
+    t.true(agent.modules.includes('https'), 'https module contains load module')
+    t.equal(agent.modules.length, 1, 'module shuld be registed')
+
+    t.end()
+})

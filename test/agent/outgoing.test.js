1c8ee11206062ab219763774f8eba236f8f39f68
강용석[느림보][Observability Platform] <yongseok.kang@navercorp.com>
2020-03-05 21:51:18 +0900

    https module support (#123)
    * HTTPS module support. In more detail, the Node.js agent can collect HTTPS outgoing requests.
    * Added rewire module to dev dependencies. You can test private functions.

---
+++ test/agent/outgoing.test.js
@@ -0,0 +1,47 @@
+const test = require('tape')
+const http = require('http')
+const echoServer = require('./support/echo-server-instance')
+const httpShared = require('../../lib/instrumentation/http-shared')
+const Agent = require('../../lib/agent')
+const PinpointHeader = require('../../lib/constant/http-header').PinpointHeader
+
+// test('http.request(options)', echoTest('http', (port, cb) => {
+//     const options = { port }
+//     const req = http.request(options)
+//     req.on('response', cb)
+//     return req
+// }))
+
+function echoTest(protocol, handler) {
+    return function(t) {
+        echoServer(protocol, (cp, port) => {
+            console.log(`cp ${cp}, port ${port}`)        
+
+            process.env.PINPOINT_AGENT_ID = 'echoTest'
+            process.env.PINPOINT_APPLICATION_NAME = 'echoTest'
+            const agent = new Agent()
+            httpShared.instrumentRequest(agent, protocol)
+            const req = handler(port, res => {
+                console.log(res)
+                res.on('end', () => {
+                })
+                res.resume()
+            })
+
+            var traceId = req.getHeader(PinpointHeader.HTTP_TRACE_ID)
+            // t.ok(traceId, 'should have a trace ID')
+            
+            if (isRequestHttpMethod(req)) req.end()
+
+            cp.kill()
+            t.end()
+        })
+    }
+
+    // Detect if the test called `http.get` (in which case outputSize should
+    // be greater than zero) or `http.request` (in which case it should equal
+    // zero)
+    function isRequestHttpMethod(req) {
+        return req.outputSize === 0
+    }
+}

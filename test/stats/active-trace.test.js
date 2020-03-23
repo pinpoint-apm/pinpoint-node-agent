d3d6226b80e5e4bb3c181c67354f3e8966818189
Yongseok Kang <yongseok.kang@navercorp.com>
2020-03-23 17:12:43 +0900

    extract method to module #125

--- test/stats/active-trace.test.js
+++ test/stats/active-trace.test.js
@@ -3,45 +3,7 @@ const axios = require('axios')
 const { fixture, util } = require('../test-helper')
 const activeTrace = require('../../lib/metric/active-trace')
 
-const mock = () => {
-  const enableDataSending = require('../test-helper').enableDataSending
-  enableDataSending()
-  const Agent = require('../../lib/agent')
-  
-  class MockPinpointClient {
-    constructor(config, agentInfo) {
-      this.mockConfig = config
-      this.mockAgentInfo = agentInfo
-      this.dataSender = {
-        sendApiMetaInfo: function() {
-  
-        },
-        sendSpan: function() {
-  
-        }
-      }
-    }
-  }
-  
-  class MockAgent extends Agent {
-    createAgentInfo(config, agentStartTime) {
-      this.mockAgentInfo = super.createAgentInfo(config, agentStartTime)
-      return this.mockAgentInfo
-    }
-    
-    startSchedule(agentId, agentStartTime) {
-      this.mockAgentId = agentId
-      this.mockAgentStartTime = agentStartTime
-    }
-  
-    initializePinpointClient(agentInfo) {
-      this.pinpointClient = new MockPinpointClient(this.config, agentInfo)
-    }
-  }
-  return new MockAgent(fixture.config)
-}
-
-const agent = mock()
+const agent = require('./agent-mock')()
 
 const express = require('express')
 
---
+++ test/stats/agent-mock.js
@@ -0,0 +1,38 @@
+const { fixture, util } = require('../test-helper')
+module.exports = () => {
+    const enableDataSending = require('../test-helper').enableDataSending
+    enableDataSending()
+    const Agent = require('../../lib/agent')
+    
+    class MockPinpointClient {
+      constructor(config, agentInfo) {
+        this.mockConfig = config
+        this.mockAgentInfo = agentInfo
+        this.dataSender = {
+          sendApiMetaInfo: function() {
+    
+          },
+          sendSpan: function() {
+    
+          }
+        }
+      }
+    }
+    
+    class MockAgent extends Agent {
+      createAgentInfo(config, agentStartTime) {
+        this.mockAgentInfo = super.createAgentInfo(config, agentStartTime)
+        return this.mockAgentInfo
+      }
+      
+      startSchedule(agentId, agentStartTime) {
+        this.mockAgentId = agentId
+        this.mockAgentStartTime = agentStartTime
+      }
+    
+      initializePinpointClient(agentInfo) {
+        this.pinpointClient = new MockPinpointClient(this.config, agentInfo)
+      }
+    }
+    return new MockAgent(fixture.config)
+  }

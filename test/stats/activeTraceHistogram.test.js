8713269b45092f404384e8fb7b138f1c58a88a87
Yongseok Kang <yongseok.kang@navercorp.com>
2020-03-23 20:42:52 +0900

    Extract module refactoring #125

--- test/stats/activeTraceHistogram.test.js
+++ test/stats/activeTraceHistogram.test.js
@@ -19,7 +19,6 @@ test(`Should get histogram`, function (t) {
     const app = new express()
   
     app.get(PATH, async (req, res) => {
-      await util.sleep(7000)
       res.send('ok get')
     })
     app.get(LASTONE_PATH, async (req, res) => {
@@ -29,10 +28,6 @@ test(`Should get histogram`, function (t) {
       t.equals(histogram.normalCount, 1)
       t.equals(histogram.slowCount, 1)
       t.equals(histogram.verySlowCount, 1)
-      for (i=0; i< 1000; i++ ) {
-        JSON.parse(req)
-      }
-      await util.sleep(1000)
       res.send('ok get')
     })
   
@@ -46,14 +41,10 @@ test(`Should get histogram`, function (t) {
       axios.get(getServerUrl(LASTONE_PATH))
   
       const agentStatsMonitor = new AgentStatsMonitor(agent.dataSender, agent.agentId, agent.agentStartTime)
-      setInterval(async () => {
-        axios.get(getServerUrl(PATH))
-        axios.get(getServerUrl(LASTONE_PATH))
-        agentStatsMonitor.send()
-        await util.sleep(2000)
-      }, 3000)
-  
-      await util.sleep(3000)
+      axios.get(getServerUrl(PATH))
+      axios.get(getServerUrl(LASTONE_PATH))
+      agentStatsMonitor.send()
+      await util.sleep(2000)
       server.close()
     })
   })

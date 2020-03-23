d354770433aa2d0e84096f7c1cca939f4457623b
Yongseok Kang <yongseok.kang@navercorp.com>
2020-03-23 22:23:38 +0900

    Extract module refactoring #125

--- test/stats/activeTraceHistogram.test.js
+++ test/stats/activeTraceHistogram.test.js
@@ -1,6 +1,7 @@
 const test = require('tape')
 const axios = require('axios')
-const { util } = require('../test-helper')
+const { util, log } = require('../test-helper')
+const activeTrace = require('../../lib/metric/active-trace')
 const AgentStatsMonitor = require('../../lib/metric/agent-stats-monitor')
 const agent = require('./agent-mock')()
 const express = require('express')
@@ -21,6 +22,7 @@ test(`Should get histogram`, function (t) {
     app.get(PATH, async (req, res) => {
       res.send('ok get')
     })
+
     app.get(LASTONE_PATH, async (req, res) => {
       const histogram = activeTrace.getCurrentActiveTraceHistogram()
       log.info(histogram)
@@ -32,18 +34,12 @@ test(`Should get histogram`, function (t) {
     })
   
     const server = app.listen(TEST_ENV.port, async function () {
-      axios.get(getServerUrl(PATH))
-      await util.sleep(2000)
-      axios.get(getServerUrl(PATH))
-      await util.sleep(2000)
-      axios.get(getServerUrl(PATH))
-      await util.sleep(2000)
-      axios.get(getServerUrl(LASTONE_PATH))
-  
-      const agentStatsMonitor = new AgentStatsMonitor(agent.dataSender, agent.agentId, agent.agentStartTime)
-      axios.get(getServerUrl(PATH))
-      axios.get(getServerUrl(LASTONE_PATH))
-      await util.sleep(2000)
+      await Promise.all([
+        axios.get(getServerUrl(PATH)),
+        axios.get(getServerUrl(PATH)),
+        axios.get(getServerUrl(PATH)),
+        axios.get(getServerUrl(LASTONE_PATH))
+      ])
       server.close()
     })
   })

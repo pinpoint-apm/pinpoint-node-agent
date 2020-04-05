9a7b14fddb8882b3453c8b969f956d3fd7727983
Yongseok Kang <yongseok.kang@navercorp.com>
2020-04-05 15:29:31 +0900

    Improvement fork agent mock   instrument hook #125

--- test/support/agent-fork-mock.js
+++ test/support/agent-fork-mock.js
@@ -1,3 +1,44 @@
 const { fixture, util } = require('../test-helper')
 const instManager = require('../../lib/instrumentation/inst-manager')
 
+module.exports = (app) => {
+    const enableDataSending = require('../test-helper').enableDataSending
+    enableDataSending()
+    const Agent = require('../../lib/agent')
+
+    class MockPinpointClient {
+        constructor(config, agentInfo) {
+            this.mockConfig = config
+            this.mockAgentInfo = agentInfo
+            this.dataSender = {
+                sendApiMetaInfo: function () {
+
+                },
+                sendSpan: function () {
+
+                },
+            }
+        }
+    }
+
+    class MockAgent extends Agent {
+        createAgentInfo(config, agentStartTime) {
+            this.mockAgentInfo = super.createAgentInfo(config, agentStartTime)
+            return this.mockAgentInfo
+        }
+
+        startSchedule(agentId, agentStartTime) {
+            this.mockAgentId = agentId
+            this.mockAgentStartTime = agentStartTime
+        }
+
+        initializePinpointClient(agentInfo) {
+            this.pinpointClient = new MockPinpointClient(this.config, agentInfo)
+        }
+
+        initailizeSupportModules() {
+            this.loadedModule = [];
+        }
+    }
+    return new MockAgent(fixture.config)
+}

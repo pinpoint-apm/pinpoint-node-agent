d1f70d3b74b1774becc752fb9ac9d3e2b8051881
Yongseok Kang <yongseok.kang@navercorp.com>
2020-04-09 14:03:01 +0900

    feat(instrumentation) learning test #127

--- test/instrumentation/fix-load-module-compatibility.test.js
+++ test/instrumentation/fix-load-module-compatibility.test.js
@@ -2,13 +2,15 @@
 
 const test = require('tape')
 const mockAgent = require('../support/agent-singleton-mock')
+const Hook = require('require-in-the-middle')
 
-test(`load async module`, (t) => {
+test(`hook.unhook() for require-in-the-middle learning test`, (t) => {
     t.plan(0)
 
-    mockAgent.resetAgent(() => {
-
+    const hook = Hook(['http'], function (exports, name, basedir) {
+        t.fail('should no call')
     })
-
+    hook.unhook()
+    require('http')
     t.end()
 })

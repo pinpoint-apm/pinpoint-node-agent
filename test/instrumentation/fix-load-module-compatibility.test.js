b1b25d07b6657c0315ca086a3667728625e102f2
Yongseok Kang <yongseok.kang@navercorp.com>
2020-04-09 14:12:57 +0900

    feat(instrumentation) learning test #127

--- test/instrumentation/fix-load-module-compatibility.test.js
+++ test/instrumentation/fix-load-module-compatibility.test.js
@@ -5,12 +5,47 @@ const mockAgent = require('../support/agent-singleton-mock')
 const Hook = require('require-in-the-middle')
 
 test(`hook.unhook() for require-in-the-middle learning test`, (t) => {
-    t.plan(0)
-
     const hook = Hook(['http'], function (exports, name, basedir) {
         t.fail('should no call')
     })
     hook.unhook()
     require('http')
     t.end()
+})
+
+test(`all modules for require-in-the-middle learning test`, (t) => {
+    t.plan(8)
+
+    let n = 1
+
+    const hook = Hook(function (exports, name, basedir) {
+        switch (n) {
+            case 1:
+                t.equal(name, 'http')
+                break
+            case 2:
+                t.equal(name, 'net')
+                break
+            default:
+                t.ok(false)
+        }
+
+        exports.foo = n++
+
+        return exports
+    })
+
+    t.on('end', function () {
+        hook.unhook()
+    })
+
+    const http = require('http')
+    const net = require('net')
+
+    t.equal(http.foo, 1)
+    t.equal(net.foo, 2)
+    t.equal(require('http').foo, 1)
+    t.deepEqual(hook.cache.get('http'), http)
+    t.deepEqual(hook.cache.get('net'), net)
+    t.equal(n, 3)
 })

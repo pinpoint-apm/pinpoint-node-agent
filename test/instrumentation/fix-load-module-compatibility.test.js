2183a7cf9b97e5195c2e974c8e27870f06b9dfdc
Yongseok Kang <yongseok.kang@navercorp.com>
2020-04-09 12:12:02 +0900

    feat(instrumentation) resetAgent for hook test #127

--- test/instrumentation/fix-load-module-compatibility.test.js
+++ test/instrumentation/fix-load-module-compatibility.test.js
@@ -1,9 +1,14 @@
 'use strict'
 
 const test = require('tape')
+const mockAgent = require('../support/agent-singleton-mock')
 
 test(`load async module`, (t) => {
-    // t.plan(1)
+    t.plan(0)
+
+    mockAgent.resetAgent(() => {
+
+    })
 
     t.end()
 })

6925b4ff70986932180682f78406aff5665ef776
yongseok kang <yongseok.kang@navercorp.com>
2020-04-27 13:32:01 +0900

    Fix redis set get function trace data used #130

---
+++ test/instrumentation/fix-async-call-stack.test.js
@@ -0,0 +1,28 @@
+const async_hooks = require('async_hooks')
+const {performance, PerformanceObserver} = require('perf_hooks')
+const set = new Set()
+const hook = async_hooks.createHook({
+  init(id, type) {
+    if (type === 'Timeout') {
+      performance.mark(`Timeout-${id}-Init`)
+      set.add(id)
+    }
+  },
+  destroy(id) {
+    if (set.has(id)) {
+      set.delete(id);
+      performance.mark(`Timeout-${id}-Destroy`)
+      performance.measure(`Timeout-${id}`,
+                          `Timeout-${id}-Init`,
+                          `Timeout-${id}-Destroy`)
+    }
+  }
+})
+hook.enable()
+const obs = new PerformanceObserver((list, observer) => {
+  console.log(list.getEntries()[0])
+  performance.clearMarks()
+  observer.disconnect()
+})
+obs.observe({ entryTypes: ['measure'], buffered: true })
+setTimeout(() => {}, 1000)

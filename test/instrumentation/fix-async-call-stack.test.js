6a4f4f268ecfad4f4f4845734300f3db5ddbf44c
yongseok kang <yongseok.kang@navercorp.com>
2020-04-27 15:39:02 +0900

    redis call stack depth #130

--- test/instrumentation/fix-async-call-stack.test.js
+++ test/instrumentation/fix-async-call-stack.test.js
@@ -1,28 +1,31 @@
-const async_hooks = require('async_hooks')
-const {performance, PerformanceObserver} = require('perf_hooks')
-const set = new Set()
-const hook = async_hooks.createHook({
-  init(id, type) {
-    if (type === 'Timeout') {
-      performance.mark(`Timeout-${id}-Init`)
-      set.add(id)
-    }
-  },
-  destroy(id) {
-    if (set.has(id)) {
-      set.delete(id);
-      performance.mark(`Timeout-${id}-Destroy`)
-      performance.measure(`Timeout-${id}`,
-                          `Timeout-${id}-Init`,
-                          `Timeout-${id}-Destroy`)
-    }
-  }
+const test = require('tape')
+const agent = require('../support/agent-singleton-mock')
+const { GenericContainer } = require("testcontainers")
+
+test(`fix redis call stack depth`, async (t) => {
+    const container = await new GenericContainer("redis")
+        .withExposedPorts(6379)
+        .start()
+
+    agent.bindHttp()
+
+    t.plan(2)
+
+    const trace = agent.createTraceObject()
+    const redis = require('redis')
+    const client = redis.createClient(
+        container.getMappedPort(6379),
+        container.getContainerIpAddress(),
+    )
+
+    client.set("key", "value", async function (error) {
+        t.true(error == null, "error is null")
+
+        const trace = agent.traceContext.currentTraceObject()
+        t.equal(trace.callStack.length, 0, "callStack is 0")
+
+        client.quit()
+        agent.completeTraceObject(trace)
+        await container.stop()
+    })
 })
-hook.enable()
-const obs = new PerformanceObserver((list, observer) => {
-  console.log(list.getEntries()[0])
-  performance.clearMarks()
-  observer.disconnect()
-})
-obs.observe({ entryTypes: ['measure'], buffered: true })
-setTimeout(() => {}, 1000)

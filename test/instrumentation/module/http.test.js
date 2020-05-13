e3672095c26554c389c52f29583be5a173805a73
Yongseok Kang <yongseok.kang@navercorp.com>
2020-05-13 23:47:58 +0900

    check nasa #129

--- test/instrumentation/module/http.test.js
+++ test/instrumentation/module/http.test.js
@@ -1,13 +1,17 @@
 const test = require('tape')
 const agent = require('../../support/agent-singleton-mock')
+const axios = require('axios')
 
 test(`outgoing request URL escape a bug`, async (t) => {
     agent.bindHttp()
 
-    t.plan(1)
+    t.plan(2)
 
     const trace = agent.createTraceObject()
     t.true(trace)
+
+    const result = await axios.get(`https://eonet.sci.gsfc.nasa.gov/api/v2.1/categories`)
+    t.true(result.status == 200)
 
     agent.completeTraceObject(trace)
 })

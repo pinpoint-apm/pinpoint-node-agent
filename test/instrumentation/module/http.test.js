807d12b7a920856b9925a6398b384824dadc8c31
Yongseok Kang <yongseok.kang@navercorp.com>
2020-05-14 12:16:16 +0900

    check dataSender #129

--- test/instrumentation/module/http.test.js
+++ test/instrumentation/module/http.test.js
@@ -5,7 +5,7 @@ const axios = require('axios')
 test(`outgoing request URL escape a bug`, async (t) => {
     agent.bindHttp()
 
-    t.plan(2)
+    t.plan(3)
 
     const trace = agent.createTraceObject()
     t.true(trace)
@@ -13,6 +13,8 @@ test(`outgoing request URL escape a bug`, async (t) => {
     axios.get(`https://eonet.sci.gsfc.nasa.gov/api/v2.1/categories`)
         .then(function (response) {
             t.true(response.status == 200)
+
+            t.true(agent.pinpointClient.dataSender.mockSpanChunk.spanEventList.length == 1, `a spanEventList`)
             agent.completeTraceObject(trace)
         })
 })

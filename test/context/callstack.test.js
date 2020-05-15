3a727aba73af2a64970f8761d58d910aa468debb
Yongseok Kang <yongseok.kang@navercorp.com>
2020-05-15 21:43:30 +0900

    axios another asyncid test #135

--- test/context/callstack.test.js
+++ test/context/callstack.test.js
@@ -1,14 +1,20 @@
 const test = require('tape')
 const agent = require('../support/agent-singleton-mock')
+const axios = require('axios')
 
 test(`span and spanEvent call stack`, async (t) => {
     agent.bindHttp()
 
-    t.plan(2)
-    
+    t.plan(3)
+
     const trace = agent.createTraceObject()
     t.equal(trace.callStack.length, 0, "callstack is 0")
     t.equal(agent.traceContext.currentTraceObject(), trace, "current trace is current asyncId trace object")
 
-    agent.completeTraceObject(trace)
+    axios.get(`https://eonet.sci.gsfc.nasa.gov/api/v2.1/categories`)
+        .then(function (response) {
+            t.true(response.status == 200)
+
+            agent.completeTraceObject(trace)
+        })
 })

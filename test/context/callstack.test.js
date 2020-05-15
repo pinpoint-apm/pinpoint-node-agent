822c987bd408b7ed8932169f260026a76bc69fd8
Yongseok Kang <yongseok.kang@navercorp.com>
2020-05-15 21:17:40 +0900

    Create Unit test #135

--- test/context/callstack.test.js
+++ test/context/callstack.test.js
@@ -4,10 +4,11 @@ const agent = require('../support/agent-singleton-mock')
 test(`span and spanEvent call stack`, async (t) => {
     agent.bindHttp()
 
-    t.plan(1)
+    t.plan(2)
     
     const trace = agent.createTraceObject()
     t.equal(trace.callStack.length, 0, "callstack is 0")
+    t.equal(agent.traceContext.currentTraceObject(), trace, "current trace is current asyncId trace object")
 
     agent.completeTraceObject(trace)
 })

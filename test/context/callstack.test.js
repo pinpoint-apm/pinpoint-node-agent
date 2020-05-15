3d188b94505dc29ded83d1199e78d1fe4eefd4ce
Yongseok Kang <yongseok.kang@navercorp.com>
2020-05-15 21:44:56 +0900

    axios another asyncid test #135

--- test/context/callstack.test.js
+++ test/context/callstack.test.js
@@ -5,7 +5,7 @@ const axios = require('axios')
 test(`span and spanEvent call stack`, async (t) => {
     agent.bindHttp()
 
-    t.plan(3)
+    t.plan(4)
 
     const trace = agent.createTraceObject()
     t.equal(trace.callStack.length, 0, "callstack is 0")
@@ -14,6 +14,7 @@ test(`span and spanEvent call stack`, async (t) => {
     axios.get(`https://eonet.sci.gsfc.nasa.gov/api/v2.1/categories`)
         .then(function (response) {
             t.true(response.status == 200)
+            t.equal(agent.traceContext.currentTraceObject(), trace, "current trace is current asyncId trace object")
 
             agent.completeTraceObject(trace)
         })

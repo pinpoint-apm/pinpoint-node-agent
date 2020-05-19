cf0b4a5e56e95b9b738900a44c3e0d4e47deb56a
yongseok kang <yongseok.kang@navercorp.com>
2020-05-19 17:27:54 +0900

    Fix async trace call #135

--- test/context/callstack.test.js
+++ test/context/callstack.test.js
@@ -19,3 +19,37 @@ test(`span and spanEvent call stack`, async (t) => {
             agent.completeTraceObject(trace)
         })
 })
+
+const express = require('express')
+const TEST_ENV = {
+    host: 'localhost',
+    port: 5006,
+}
+const getServerUrl = (path) => `http://${TEST_ENV.host}:${TEST_ENV.port}${path}`
+//https://***REMOVED***/issues/135
+test(`fix express call stack depth`, async (t) => {
+    agent.bindHttp()
+
+    t.plan(1)
+
+    const app = new express()
+    const path = `/`
+
+    app.use(express.json())
+    app.use(express.urlencoded({
+        extended: false
+    }))
+
+    const router1 = express.Router()
+    router1.get(path, (req, res) => {
+        res.send('ok router1')
+    })
+
+    app.use('/router1', router1)
+    
+    const server = app.listen(TEST_ENV.port, async function () {
+        const result1 = await axios.get(getServerUrl(`/router1${path}`))
+        t.ok(result1.status, 200)
+        server.close()
+    })
+})

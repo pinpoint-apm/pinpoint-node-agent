d66f4568732325ce1b17f698503cb9cadc15ee09
Yongseok Kang <yongseok.kang@navercorp.com>
2020-05-20 17:38:23 +0900

    outgoing request annotations #138

--- test/context/callstack.test.js
+++ test/context/callstack.test.js
@@ -5,7 +5,7 @@ const axios = require('axios')
 test(`span and spanEvent call stack`, async (t) => {
     agent.bindHttp()
 
-    t.plan(4)
+    t.plan(5)
 
     const trace = agent.createTraceObject()
     t.equal(trace.callStack.length, 0, "callstack is 0")
@@ -15,7 +15,7 @@ test(`span and spanEvent call stack`, async (t) => {
         .then(function (response) {
             t.true(response.status == 200)
             t.equal(agent.traceContext.currentTraceObject(), trace, "current trace is current asyncId trace object")
-
+            t.equal(agent.pinpointClient.dataSender.mockSpanChunk.spanEventList.length, 1, "spanEventList length")
             agent.completeTraceObject(trace)
         })
 })

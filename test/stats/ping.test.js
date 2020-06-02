fe27a3b4ca4a428d20395b60a1b4a108d9657679
yongseok kang <yongseok.kang@navercorp.com>
2020-06-02 15:58:50 +0900

    sendPing Unit test #150

--- test/stats/ping.test.js
+++ test/stats/ping.test.js
@@ -1,10 +1,15 @@
 const test = require('tape')
-const agent = require('../support/agent-singleton-mock')
+const PinpointClient = require('../../lib/client/pinpoint-client')
+const dataSenderMock = require(`../support/data-sender-mock`)
+const { fixture, log } = require('../test-helper')
 
 test(`send ping interval`, async (t) => {
-    agent.bindHttp()
-
     t.plan(1)
 
-    t.true(agent.pinpointClient.dataSender.pingCount > 0, `agent ping schedule`)
+    const config = fixture.config
+    const client = new PinpointClient(config, {
+        agentId: config.agentId
+    })
+    client.dataSender = dataSenderMock()
+    t.true(client.dataSender.pingCount > 0, `agent ping schedule`)
 })

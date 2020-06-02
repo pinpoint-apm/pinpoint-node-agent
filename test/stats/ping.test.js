c7b83138a8c1bd36053074a440803f2b6c4540dc
yongseok kang <yongseok.kang@navercorp.com>
2020-06-02 15:38:21 +0900

    sendPing Unit test #150

--- test/stats/ping.test.js
+++ test/stats/ping.test.js
@@ -1,2 +1,10 @@
 const test = require('tape')
+const agent = require('../support/agent-singleton-mock')
 
+test(`send ping interval`, async (t) => {
+    agent.bindHttp()
+
+    t.plan(1)
+
+    t.true(agent.pinpointClient.dataSender.pingCount > 0, `agent ping schedule`)
+})

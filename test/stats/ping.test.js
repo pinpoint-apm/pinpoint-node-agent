67f580647334ece7f8427dca50ea8da573a698c1
Yongseok Kang <yongseok.kang@navercorp.com>
2020-06-06 20:54:04 +0900

    Fix Ping payload #156

--- test/stats/ping.test.js
+++ test/stats/ping.test.js
@@ -3,6 +3,8 @@ const PinpointClient = require('../../lib/client/pinpoint-client')
 const dataSenderMock = require(`../support/data-sender-mock`)
 const { fixture, log } = require('../test-helper')
 const DataSender = require('../../lib/client/data-sender')
+const {PacketType} = require('../../lib/client/packet/packet-type')
+const PingPacket = require('../../lib/client/packet/ping-packet')
 const agent = require('../support/agent-singleton-mock')
 
 test(`send ping interval`, async (t) => {
@@ -38,4 +40,10 @@ test(`sendStringMetaInfo, sendAgentInfo, sendApiMetaInfo`, (t) => {
         t.true(true, `sendApiMetaInfo use send method`)
     }
     dataSender.sendApiMetaInfo({})
+})
+
+test(`Ping packet type`, (t) => {
+    t.plan(1)
+    const packet = new PingPacket()
+    t.equal(packet.getPacketType(), PacketType.CONTROL_PING_PAYLOAD, 'packet type is payload')
 })

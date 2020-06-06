90cdcb6562054ff43eecf062cc9af927daa84a66
Yongseok Kang <yongseok.kang@navercorp.com>
2020-06-06 17:47:50 +0900

    sendAgentInfo use a tcp socket. #156

--- test/stats/ping.test.js
+++ test/stats/ping.test.js
@@ -2,6 +2,8 @@ const test = require('tape')
 const PinpointClient = require('../../lib/client/pinpoint-client')
 const dataSenderMock = require(`../support/data-sender-mock`)
 const { fixture, log } = require('../test-helper')
+const DataSender = require('../../lib/client/data-sender')
+const agent = require('../support/agent-singleton-mock')
 
 test(`send ping interval`, async (t) => {
     t.plan(1)
@@ -13,4 +15,17 @@ test(`send ping interval`, async (t) => {
     client.dataSender = dataSenderMock()
     t.true(client.dataSender.pingCount > 0, `agent ping schedule`)
     client.ping.scheduler.stop()
+})
+
+test(`sendStringMetaInfo, sendAgentInfo, sendApiMetaInfo`, (t) => {
+    agent.bindHttp()
+    
+    const config = fixture.config
+    const dataSender = new DataSender(config)
+    t.plan(1)
+
+    dataSender.tcpClient.send = () => {
+        t.true(true, `sendAgentInfo use send method`)
+    }
+    dataSender.sendAgentInfo(agent.mockAgentInfo)
 })

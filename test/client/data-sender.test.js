b9c0c6e373502a9cb2c4d331c8658ffbb3418cf5
Yongseok Kang <yongseok.kang@navercorp.com>
2020-06-10 14:24:16 +0900

    grpc #156

--- test/client/data-sender.test.js
+++ test/client/data-sender.test.js
@@ -5,32 +5,22 @@ const axios = require('axios')
 const { fixture } = require('../test-helper')
 const Trace = require('../../lib/context/trace')
 const dataSenderMock = require('../support/data-sender-mock')
-const dataSender = {
-  sendSpan: function(span) {
-    this.mockSpan = span
-  },
-  sendAgentInfo: function(agentInfo) {
-    this.mockAgentInfo = agentInfo
-  },
-  sendStringMetaInfo: function(metaInfo) {
-    this.mockMetaInfo = metaInfo
-  }
-}
+const dataSender = dataSenderMock()
+const AgentInfo = require('../../lib/data/dto/agent-info')
+const agentInfo = AgentInfo.create(fixture.config, Date.now())
+const MethodDescriptor = require('../../lib/context/method-descriptor')
+const MethodType = require('../../lib/constant/method-type').MethodType
+const ApiMetaInfo = require('../../lib/data/dto/api-meta-info')
+const StringMetaInfo = require('../../lib/data/dto/string-meta-info')
 
 // const { log, fixture, util, enableDataSending } = require('../test-helper')
 // enableDataSending()
 
 // const Trace = require('../../lib/context/trace')
-// const AgentInfo = require('../../lib/data/dto/agent-info')
-// const ApiMetaInfo = require('../../lib/data/dto/api-meta-info')
-// const StringMetaInfo = require('../../lib/data/dto/string-meta-info')
-// const MethodDescriptor = require('../../lib/context/method-descriptor')
-// const MethodType = require('../../lib/constant/method-type').MethodType
 // const dataSenderFactory = require('../../lib/client/data-sender-factory')
 
 // const GRPC_ENABLE = false
 // fixture.config['grpcEnable'] = GRPC_ENABLE
-// const agentInfo = AgentInfo.create(fixture.config, Date.now())
 // const dataSender = dataSenderFactory.create(fixture.config, agentInfo)
 
 test('Should send agent info', function (t) {
@@ -48,7 +38,7 @@ test('Should send api meta info', function (t) {
   const apiMetaInfo = ApiMetaInfo.create(methodDescriptor)
   dataSender.send(apiMetaInfo)
 
-  t.equal(dataSender.mockSpan, span, "span is equal in datasender")
+  t.equal(dataSender.mockAPIMetaInfo, apiMetaInfo, "span is equal in datasender")
 })
 
 test('Should send string meta info', function (t) {
@@ -57,11 +47,12 @@ test('Should send string meta info', function (t) {
   const stringMetaInfo = StringMetaInfo.create('1', 'test string')
   dataSender.send(stringMetaInfo)
 
-  t.deepEqual(dataSender.mockAgentInfo, fixture.getAgentInfo(), "agentInfo is equal in datasender")
+  t.deepEqual(dataSender.mockMetaInfo, stringMetaInfo, "agentInfo is equal in datasender")
 })
 
 test('Should send string meta info', function (t) {
   t.plan(1)
+  dataSender.mockSpan = null
 
   const traceId = fixture.getTraceId()
   const agentInfo = fixture.getAgentInfo()
@@ -73,5 +64,5 @@ test('Should send string meta info', function (t) {
 
   dataSender.send(span)
 
-  t.equal(dataSender.mockMetaInfo, metaInfo, "metaInfo is equal in datasender")
+  t.equal(dataSender.mockSpan, span, "metaInfo is equal in datasender")
 })
--- test/support/data-sender-mock.js
+++ test/support/data-sender-mock.js
@@ -1,22 +1,40 @@
 'use strict'
 
 const { fixture, util, log } = require('../test-helper')
+const AgentInfo = require('../../lib/data/dto/agent-info')
+const ApiMetaInfo = require('../../lib/data/dto/api-meta-info')
+const StringMetaInfo = require('../../lib/data/dto/string-meta-info')
+const Span = require('../../lib/context/span')
 
 const dataSender = () => {
-    return {
-        sendApiMetaInfo: function(apiMetaInfo) {
-          this.mockAPIMetaInfo = apiMetaInfo
-        },
-        sendSpan: function(span) {
-          this.mockSpan = span
-        },
-        sendSpanChunk: function(spanChunk) {
-          this.mockSpanChunk = spanChunk
-        },
-        sendStringMetaInfo: function(metaInfo) {
-          this.mockMetaInfo = metaInfo
-        }
+  return {
+    sendApiMetaInfo: function (apiMetaInfo) {
+      this.mockAPIMetaInfo = apiMetaInfo
+    },
+    sendSpan: function (span) {
+      this.mockSpan = span
+    },
+    sendSpanChunk: function (spanChunk) {
+      this.mockSpanChunk = spanChunk
+    },
+    sendStringMetaInfo: function (metaInfo) {
+      this.mockMetaInfo = metaInfo
+    },
+    sendAgentInfo: function(agentInfo) {
+      this.agentInfo = agentInfo
+    },
+    send: function (data) {
+      if (data instanceof AgentInfo) {
+        this.sendAgentInfo(data)
+      } else if (data instanceof ApiMetaInfo) {
+        this.sendApiMetaInfo(data)
+      } else if (data instanceof StringMetaInfo) {
+        this.sendStringMetaInfo(data)
+      } else if (data instanceof Span) {
+        this.sendSpan(data)
+      }
     }
+  }
 }
 
 module.exports = dataSender

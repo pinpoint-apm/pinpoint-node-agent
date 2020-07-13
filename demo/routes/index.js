4f80288bc3325de338b2f30bfd0bc7ad06bfc65a
yongseok kang <yongseok.kang@navercorp.com>
2020-07-13 12:40:21 +0900

    IORedis  #141

--- demo/routes/index.js
+++ demo/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const IORedis = require('ioredis');
+const ioRedis = new IORedis(6379)
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });

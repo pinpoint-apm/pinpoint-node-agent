351fed0def48d8cec67fd21818afb4f5651c1a3e
Yongseok Kang <yongseok.kang@navercorp.com>
2020-07-17 13:51:02 +0900

    PM2 sample configuration #173

--- demo/ecosystem.config.js
+++ demo/ecosystem.config.js
@@ -1,21 +1,20 @@
 module.exports = {
   apps : [{
     name: 'agent',
-    script: 'app.js',
+    script: './bin/www',
 
     // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-    args: 'one two',
-    instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
+    max_restarts: 10,
     env: {
       NODE_ENV: 'development',
-      "PINPOINT_ENABLE": "true",
       "PINPOINT_CONTAINER": "false",
+      "PINPOINT_AGENT_ID": "express-node-js-id",
       "PINPOINT_APPLICATION_NAME": "express.node.js",
-      "PINPOINT_COLLECTOR_IP": "dev.collector.pinpoint.navercorp.com",
-      "PINPOINT_LOG_LEVEL": "DEBUG"      
+      "PINPOINT_COLLECTOR_IP": "localhost",
+      "PINPOINT_LOG_LEVEL": "DEBUG"
     },
     env_production: {
       NODE_ENV: 'production'

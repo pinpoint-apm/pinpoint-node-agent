8a8a179484909ea16fd339d4ce22a2886d8c2acb
Yongseok Kang <yongseok.kang@navercorp.com>
2020-07-17 17:08:49 +0900

    PM2 sample configuration #173

--- demo/ecosystem.config.js
+++ demo/ecosystem.config.js
@@ -7,17 +7,18 @@ module.exports = {
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
-    max_restarts: 10,
+    instances: 0,
     env: {
       NODE_ENV: 'development',
       "PINPOINT_CONTAINER": "false",
       "PINPOINT_AGENT_ID": "express-node-js-id",
       "PINPOINT_APPLICATION_NAME": "express.node.js",
       "PINPOINT_COLLECTOR_IP": "localhost",
-      "PINPOINT_LOG_LEVEL": "DEBUG"
+      "PINPOINT_LOG_LEVEL": "DEBUG",
+      // "PINPOINT_SAMPLING_RATE": "1",
     },
     env_production: {
-      NODE_ENV: 'production'
+      NODE_ENV: 'production',
     }
   }],
 };

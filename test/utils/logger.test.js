b385af92110b7f4f2d8b6769e69de417cf3ea39d
yongseok kang <yongseok.kang@navercorp.com>
2020-07-20 15:01:02 +0900

    debug query method #171

---
+++ test/utils/logger.test.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const test = require('tape')
+const log = require('../../lib/utils/logger')
+
+test('isDebug', (t) => {
+    t.plan(2)
+    t.equal(log.isDebug(), false, 'debug null')
+
+    log.init("DEBUG")
+    t.equal(log.isDebug(), true, 'debug')
+})

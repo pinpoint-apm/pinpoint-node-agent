ffdbdc656555ebebe581c5fd8aa5d952c1a6c8bf
Yongseok Kang <yongseok.kang@navercorp.com>
2020-09-10 14:43:33 +0900

    Spring boot demo #189

--- demo/springboot/src/main/java/com/example/springboot/HelloController.java
+++ demo/springboot/src/main/java/com/example/springboot/HelloController.java
@@ -3,12 +3,29 @@ package com.example.springboot;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.IOException;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 @RestController
 public class HelloController {
 
 	@RequestMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
-	}
+	public String index() throws IOException {
+        String result = this.run("http://localhost:3000");
+		return result;
+    }
+    
+    OkHttpClient client = new OkHttpClient();
 
+    String run(String url) throws IOException {
+        Request request = new Request.Builder()
+            .url(url)
+            .build();
+
+        try (Response response = client.newCall(request).execute()) {
+        return response.body().string();
+        }
+    }
 }

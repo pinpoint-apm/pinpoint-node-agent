4e397f0584fb8b69092b047ff2272b18a9b83e83
Yongseok Kang <yongseok.kang@navercorp.com>
2020-11-12 22:24:42 +0900

    Node version check https://github.com/pinpoint-apm/pinpoint-node-agent/issues/14

--- test/metric/resource-stats-collector.test.js
+++ test/metric/resource-stats-collector.test.js
@@ -15,11 +15,23 @@ test(`detect Node 8, core-js, version`, (t) => {
         return '8.9.3'
     }
     v8Compatibility.initialize()
-    t.true(v8Compatibility.disabled, '8.9.3', "8.9.3 disable version")
+    t.true(v8Compatibility.disabled, "8.9.3 disable version")
 
+    v8Compatibility.getNodeVersion = () => {
+        return '0.3.4'
+    }
+    v8Compatibility.initialize()
+    t.true(v8Compatibility.disabled, "0.3.4 disable version")
+
+    v8Compatibility.getNodeVersion = () => {
+        return '9.0.0'
+    }
+    v8Compatibility.initialize()
+    t.true(v8Compatibility.disabled, "0.3.4 disable version")
 
     v8Compatibility.getNodeVersion = origin
     v8Compatibility.initialize()
     t.equal(v8Compatibility.getNodeVersion(), process.versions.node)
+    t.false(v8Compatibility.disabled(), `${process.versions.node} v8 enabled version`)
     t.end()
 })

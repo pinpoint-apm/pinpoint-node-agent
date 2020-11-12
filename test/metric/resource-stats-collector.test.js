c9ebad2bbed2bcbd438a73d30b2005fe72d6dd0c
Yongseok Kang <yongseok.kang@navercorp.com>
2020-11-12 16:46:21 +0900

    Node version check https://github.com/pinpoint-apm/pinpoint-node-agent/issues/14

--- test/metric/resource-stats-collector.test.js
+++ test/metric/resource-stats-collector.test.js
@@ -6,8 +6,19 @@
 
 const test = require('tape')
 const v8Compatibility = require('../../lib/metric/v8-compatibility')
+const { getNodeVersion } = require('../../lib/metric/v8-compatibility')
 
 test(`detect Node 8, core-js, version`, (t) => {
-    t.equal(v8Compatibility.version, process.version, "current node version")
+    const origin = v8Compatibility.getNodeVersion
+
+    v8Compatibility.getNodeVersion = () => {
+        return '8.9.3'
+    }
+    v8Compatibility.initialize()
+    t.equal(v8Compatibility.version, '8.9.3', "current node version")
+    v8Compatibility.getNodeVersion = origin
+
+    v8Compatibility.initialize()
+    t.notEqual(v8Compatibility.version, '8.9.3')
     t.end()
 })

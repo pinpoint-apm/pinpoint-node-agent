2a4d574218e404ae9af3a4e7d9e3eadf0efb2fe0
Yongseok Kang <feelform@gmail.com>
2020-11-17 16:59:51 +0900

    matcher #17

--- test/utils/ant-path-matcher.test.js
+++ test/utils/ant-path-matcher.test.js
@@ -10,7 +10,12 @@ const AntPathMatcher = require('../../lib/utils/ant-path-matcher')
 // https://github.com/spring-projects/spring-framework/blob/master/spring-core/src/test/java/org/springframework/util/AntPathMatcherTests.java
 test('Unit test for AntPathMatcher', (t) => {
     const pathMatcher = new AntPathMatcher()
+
+    // test exact matching
     t.ok(pathMatcher, 'Ant Path matcher initialization')
     t.true(pathMatcher.match("test", "test"))
+    t.true(pathMatcher.match("/test", "/test"))
+
+    // SPR-14141
     t.end()
 })

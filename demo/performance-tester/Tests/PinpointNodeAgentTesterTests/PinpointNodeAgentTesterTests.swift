054e0776ed91a50d4a67aae31a54705953157f2c
Yongseok Kang <yongseok.kang@navercorp.com>
2020-12-22 23:37:50 +0900

    gRPC channel test

--- demo/performance-tester/Tests/PinpointNodeAgentTesterTests/PinpointNodeAgentTesterTests.swift
+++ demo/performance-tester/Tests/PinpointNodeAgentTesterTests/PinpointNodeAgentTesterTests.swift
@@ -3,6 +3,7 @@ import Foundation
 import Combine
 @testable import PinpointNodeAgentTester
 
+@available(OSX 11.0, *)
 final class PinpointNodeAgentTesterTests: XCTestCase {
     var subscriptions = Set<AnyCancellable>()
     
@@ -17,6 +18,32 @@ final class PinpointNodeAgentTesterTests: XCTestCase {
         .store(in: &subscriptions)
         
         waitForExpectations(timeout: 30)
+    }
+    
+    func testGrpcChannel() {
+        let exp = expectation(description: "grpc channel test")
+        
+        let tester = PinpointNodeAgentTester()
+        let timer = Timer
+                .publish(every: 1.0, on: .main, in: .common)
+                .autoconnect()
+        let subscription = timer
+            .flatMap({ index -> AnyPublisher<String, PinpointNodeAgentTester.Error> in
+                return tester.request()
+            })
+            .scan(0) { index, _ in index + 1 }
+            .sink(receiveCompletion: { result in
+                print("receiveCompletion: \(result)")
+            }, receiveValue: { data in
+                print("receiveValue: \(data)")
+                
+                if data > 10 {
+                    timer.upstream.connect().cancel()
+                }
+            })
+        subscription.store(in: &subscriptions)
+            
+        waitForExpectations(timeout: 2 * 24 * 60 * 60)
     }
 
     static var allTests = [

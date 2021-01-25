8d005c08e8ccd1160ce6bba8ec57fce8954b50f5
Yongseok Kang <yongseok.kang@navercorp.com>
2021-01-25 13:45:00 +0900

    call stat test code #33

--- test/client/grpc-stream.test.js
+++ test/client/grpc-stream.test.js
@@ -9,17 +9,19 @@ const grpc = require('@grpc/grpc-js')
 
 const services = require('../../lib/data/grpc/Service_grpc_pb')
 const messages = require('../../lib/data/grpc/Service_pb')
+const dataConvertor = require('../../lib/data/grpc-data-convertor')
 
 function sendAgentStat(call, callback) {
     console.log('call request: ' + call.request)
 }
 
+const agentStartTime = Date.now()
 const headerInterceptor = function (options, nextCall) {
     return new grpc.InterceptingCall(nextCall(options), {
         start: function (metadata, listener, next) {
             metadata.add('agentid', '1212121212')
             metadata.add('applicationname', 'test')
-            metadata.add('starttime', String(Date.now()))
+            metadata.add('starttime', String(agentStartTime))
             next(metadata, listener, next)
         },
     })
@@ -32,6 +34,25 @@ function callStat() {
             interceptors: [headerInterceptor]
         }
     )
+
+    const call = statClient.sendAgentStat((err, response) => {
+        if (err) {
+            console.log(`statStream callback err: ${err}`)
+        }
+    })
+    // agent-stats-monitor.js
+    const pStatMessage = dataConvertor.convertStat({
+        agentId: '1212121212',
+        agentStartTime: agentStartTime,
+        timestamp: Date.now(),
+        collectInterval: 1000,
+        memory: 0,
+        cpu: {
+            user: 0,
+            system: 0
+        }
+    })
+    call.write(pStatMessage)
 }
 
 test('client side streaming', function (t) {

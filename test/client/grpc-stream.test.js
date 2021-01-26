63da090c899c4d0bba43caf06809efde51b68672
Yongseok Kang <yongseok.kang@navercorp.com>
2021-01-26 15:01:42 +0900

    support loop test #33

--- test/client/grpc-stream.test.js
+++ test/client/grpc-stream.test.js
@@ -14,7 +14,6 @@ const { Empty } = require('google-protobuf/google/protobuf/empty_pb')
 
 // https://github.com/agreatfool/grpc_tools_node_protoc_ts/blob/7caf9fb3a650fe7cf7a04c0c65201997874a5f38/examples/src/grpcjs/server.ts#L53
 function sendAgentStat(call, callback) {
-    console.log('call request: ' + call.request)
     call.on('data', function(stat) {
         console.log('call request stat: ' + stat)
     })

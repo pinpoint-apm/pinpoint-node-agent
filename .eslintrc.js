5cdf6e90f5b7693a7ec3d478812b902df8ffa509
Yongseok Kang <yongseok.kang@navercorp.com>
2021-02-03 12:04:26 +0900

    #37 checkstyle support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,28 @@
-{
-  "extends": ["airbnb"],
-  "settings": {
-    "import/resolver": {
-      "node": {
-        "paths": ["lib"]
-      }
+module.exports = {
+    'env': {
+        'commonjs': true,
+        'es2021': true
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 12
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ]
     }
-  }
 }

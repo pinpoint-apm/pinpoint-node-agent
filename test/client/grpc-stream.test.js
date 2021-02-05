66cb5eb0354533a4cec25a012c3e478909883586
Yongseok Kang <yongseok.kang@navercorp.com>
2021-02-05 22:18:21 +0900

    #40 addline test

--- test/client/grpc-stream.test.js
+++ test/client/grpc-stream.test.js
@@ -8,7 +8,6 @@ const test = require('tape')
 const grpc = require('@grpc/grpc-js')
 
 const services = require('../../lib/data/grpc/Service_grpc_pb')
-const messages = require('../../lib/data/grpc/Service_pb')
 const dataConvertor = require('../../lib/data/grpc-data-convertor')
 const { Empty } = require('google-protobuf/google/protobuf/empty_pb')
 

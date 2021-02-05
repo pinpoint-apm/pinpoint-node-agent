107e83b177ed8447c8d75ebf3f9eb4bcff1796e8
Yongseok Kang <yongseok.kang@navercorp.com>
2021-02-05 22:22:34 +0900

    #40 Fix Node env

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     'env': {
         'commonjs': true,
-        'es2021': true
+        'es2021': true,
+        "node": true
     },
     'extends': 'eslint:recommended',
     'parserOptions': {

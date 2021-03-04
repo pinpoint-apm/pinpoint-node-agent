ac14bfb36c523ca0c68d70703be6bc60358e8f27
Yongseok Kang <yongseok.kang@navercorp.com>
2021-03-04 14:44:33 +0900

    #35 Unit test for modules with versions

---
+++ test/client/inst-manager.test.js
@@ -0,0 +1,13 @@
+/**
+ * Pinpoint Node.js Agent
+ * Copyright 2021-present NAVER Corp.
+ * Apache License v2.0
+ */
+
+
+const test = require('tape')
+const { log } = require('../test-helper')
+
+test('load modules with versions', (t) => {
+    t.end()
+})

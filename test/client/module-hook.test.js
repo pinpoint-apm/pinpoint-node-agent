266f1b763075ecd60feb3b4d665ba9cc6bf80590
Yongseok Kang <yongseok.kang@navercorp.com>
2021-03-09 00:31:55 +0900

    #35 version matcher

--- test/client/module-hook.test.js
+++ test/client/module-hook.test.js
@@ -13,10 +13,18 @@ test('load modules with versions', (t) => {
     t.equal(http1, http2, 'Hook modules caches http')
     t.false(agent.moduleHook.needLoadModules('http'), 'needLoadModules(http) is false')
 
+    const http = agent.moduleHook.moduleObj('http')
+    t.equal(http.name, 'http', 'module http name matches')
+    t.equal(http.version, process.versions.node, 'module http version matches with node version')
+
     const redis1 = require('redis')
     const redis2 = require('redis')
     t.equal(redis1, redis2, 'Hook modules caches redis')
     t.false(agent.moduleHook.needLoadModules('redis'), 'needLoadModules(redis) is false')
+
+    const redis = agent.moduleHook.moduleObj('redis')
+    t.equal(redis.name, 'redis', 'module redis name matches')
+    t.equal(redis.version, '2.8.0', 'module redis version matches with package dependency version')
 
     t.end()
 })

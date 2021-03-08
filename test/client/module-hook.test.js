ebe2f9802b06f3d144d1d9cc9f0baae339d1a95a
Yongseok Kang <yongseok.kang@navercorp.com>
2021-03-09 00:15:34 +0900

    #35 store modules with version

--- test/client/module-hook.test.js
+++ test/client/module-hook.test.js
@@ -11,13 +11,11 @@ test('load modules with versions', (t) => {
     const http1 = require('http')
     const http2 = require('http')
     t.equal(http1, http2, 'Hook modules caches http')
-    t.true(agent.moduleHook.isLoadedModule('http'), 'isLoadedModule(http) is true')
     t.false(agent.moduleHook.needLoadModules('http'), 'needLoadModules(http) is false')
 
     const redis1 = require('redis')
     const redis2 = require('redis')
     t.equal(redis1, redis2, 'Hook modules caches redis')
-    t.true(agent.moduleHook.isLoadedModule('redis'), 'isLoadedModule(redis) is true')
     t.false(agent.moduleHook.needLoadModules('redis'), 'needLoadModules(redis) is false')
 
     t.end()

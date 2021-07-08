a73fe6b2e9553b87e40ab807173ff1fff6d7c63f
Yongseok Kang <yongseok.kang@navercorp.com>
2021-07-08 18:41:51 +0900

    #86 app directory

--- test/context/make-method-descriptor-builder.test.js
+++ test/context/make-method-descriptor-builder.test.js
@@ -5,9 +5,14 @@
  */
 
 const test = require('tape')
-const { makeMethodDescriptorBuilderByFileName } = require('../../lib/context/make-method-descriptor-builder')
+const { methodDescriptorBuilder } = require('../../lib/context/make-method-descriptor-builder')
 
 test('makeMethodDescriptorBuilderByFileName', (t) => {
-    const actual = makeMethodDescriptorBuilderByFileName(1)
+    const actual = methodDescriptorBuilder(undefined, 'at Object.Module._extensions..js (internal/modules/cjs/loader.js:699:10)')
+    t.equal(actual.className, 'Object', 'className')
+    t.equal(actual.fileName, 'loader.js', 'fileName')
+    t.equal(actual.functionName, 'Module._extensions..js')
+    t.equal(actual.lineNumber, 699, 'lineNumber')
+    t.equal(actual.location, 'internal/modules/cjs/')
     t.end()
 })

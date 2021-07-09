38e49a29aac3b4afbdce1b884a3d60e8c1ea6211
Yongseok Kang <yongseok.kang@navercorp.com>
2021-07-09 21:57:57 +0900

    #86 APIInfo

--- test/context/make-method-descriptor-builder.test.js
+++ test/context/make-method-descriptor-builder.test.js
@@ -8,14 +8,15 @@ const test = require('tape')
 const { methodDescriptorBuilder } = require('../../lib/context/make-method-descriptor-builder')
 
 test('makeMethodDescriptorBuilderByFileName', (t) => {
-    const actual = methodDescriptorBuilder(undefined, 'at Object.Module._extensions..js (internal/modules/cjs/loader.js:699:10)')
+    let actual = methodDescriptorBuilder(undefined, 'at Object.Module._extensions..js (internal/modules/cjs/loader.js:699:10)')
     t.equal(actual.className, 'Object', 'className')
     t.equal(actual.fileName, 'loader.js', 'fileName')
     t.equal(actual.functionName, 'Module._extensions..js')
     t.equal(actual.lineNumber, 699, 'lineNumber')
     t.equal(actual.location, 'internal/modules/cjs/')
 
-    const actualMethodDescriptor = actual.build()
+    let actualMethodDescriptor = actual.build()
     t.equal(actualMethodDescriptor.getAPIInfo(), 'Object.Module._extensions..js (internal/modules/cjs/loader.js)')
+
     t.end()
 })

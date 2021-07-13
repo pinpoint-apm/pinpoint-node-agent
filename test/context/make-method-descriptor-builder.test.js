4b662f89ee5323c5f00c8f8e5ab3a38274072906
Yongseok Kang <yongseok.kang@navercorp.com>
2021-07-13 20:47:23 +0900

    #86 support new functionName

--- test/context/make-method-descriptor-builder.test.js
+++ test/context/make-method-descriptor-builder.test.js
@@ -5,22 +5,29 @@
  */
 
 const test = require('tape')
-const { methodDescriptorBuilder } = require('../../lib/context/make-method-descriptor-builder')
+const { captureNamedGroup, methodDescriptorBuilder } = require('../../lib/context/make-method-descriptor-builder')
+const MethodDescriptorBuilder = require('../../lib/context/method-descriptor-builder')
 
 test('makeMethodDescriptorBuilderByFileName', (t) => {
-    let actual = methodDescriptorBuilder(undefined, 'at Object.Module._extensions..js (internal/modules/cjs/loader.js:699:10)')
+    let actual = captureNamedGroup('at new functionName (internal/modules/cjs/loader.js:699:10)')
+    t.equal(actual.className, undefined, 'className')
+    t.equal(actual.fileName, 'loader.js', 'fileName')
+    t.equal(actual.functionName, 'FunctionName')
+    t.equal(actual.lineNumber, '699', 'lineNumber')
+    t.equal(actual.location, 'internal/modules/cjs/')
+
+    let actualMethodDescriptor = MethodDescriptorBuilder.make(undefined, actual).build()
+    t.equal(actualMethodDescriptor.getAPIInfo(), 'FunctionName (internal/modules/cjs/loader.js)')
+
+    actual = methodDescriptorBuilder(undefined, 'at Object.Module._extensions..js (internal/modules/cjs/loader.js:699:10)')
     t.equal(actual.className, 'Object', 'className')
     t.equal(actual.fileName, 'loader.js', 'fileName')
     t.equal(actual.functionName, 'Module._extensions..js')
     t.equal(actual.lineNumber, 699, 'lineNumber')
     t.equal(actual.location, 'internal/modules/cjs/')
 
-    let actualMethodDescriptor = actual.build()
+    actualMethodDescriptor = actual.build()
     t.equal(actualMethodDescriptor.getAPIInfo(), 'Object.Module._extensions..js (internal/modules/cjs/loader.js)')
-
-    actual = methodDescriptorBuilder(undefined, 'at new functionName (internal/modules/cjs/loader.js:699:10)')
-    t.false(actual)
-    // t.equal(actual.className, 'Object', 'className')
 
     t.end()
 })

fa9177e67eb3177fe88a95cb01b3486fc77d9681
Yongseok Kang <feelform@gmail.com>
2023-01-04 16:08:34 +0900

    [#125] mysql module support

---
+++ test/instrumentation/module/mysql.test.js
@@ -0,0 +1,36 @@
+/**
+ * Pinpoint Node.js Agent
+ * Copyright 2023-present NAVER Corp.
+ * Apache License v2.0
+ */
+
+const test = require('tape')
+const { MySqlContainer } = require("testcontainers")
+const mysql = require('mysql')
+
+test(`MySql Query`, async (t) => {
+    const container = await new MySqlContainer('mysql:5.7.40').start()
+
+    const connection = mysql.createConnection({
+        host: container.getHost(),
+        port: container.getPort(),
+        database: container.getDatabase(),
+        user: container.getUsername(),
+        password: container.getUserPassword(),
+    })
+    connection.connect(function (err) {
+        if (err) {
+            console.error('error connecting: ' + err.stack)
+            return
+        }
+        console.log('connected as id ' + connection.threadId)
+    })
+    connection.query('SELECT 1 as res', async function (error, results, fields) {
+        if (error) throw error
+        t.equal(results[0].res, 1, 'rows SELECT 1 as res')
+
+        connection.end()
+        await container.stop()
+        t.end()
+    })
+})

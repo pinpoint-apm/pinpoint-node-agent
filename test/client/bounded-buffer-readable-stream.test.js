4568a725a77f9de9b08b06daf6bae1176a8640fd
Yongseok <feelform@gmail.com>
2024-03-26 21:38:41 +0900

    * writableFactory for reconnect gRPC writableSteam

--- test/client/bounded-buffer-readable-stream.test.js
+++ test/client/bounded-buffer-readable-stream.test.js
@@ -50,19 +50,48 @@ test('piped readable steam', (t) => {
 })
 
 test('reconnect piped readable steam', (t) => {
+    t.plan(9)
     const readable = new BoundedBufferReadableStream({ encoding: 'utf8' })
     readable.push('test1')
     readable.push('test2')
-    readable.push(null)
 
-    const actualSteams = []
     readable.pipe(() => {
-        return new Writable({
+        let count = 0
+        const actualSteams = []
+        const writableStream = new Writable({
             write(chunk, encoding, callback) {
                 actualSteams.push(chunk.toString())
                 callback()
+                count++
+                
+                if (count === 2) {
+                    writableStream.destroy(new Error('writable steam occurred an error'))
+                }
             }
         })
+        writableStream.on('error', (error) => {
+            t.equal(error.message, 'writable steam occurred an error', 'piped readable steam error')
+            t.equal(readable.readableSteam.readable, true, 'piped readable steam is readable on close event')
+            t.equal(readable.buffer.length, 0, 'piped readable steam buffer is empty')
+    
+            t.equal(actualSteams.length, 2, 'piped readable steam')
+            t.equal(actualSteams[0], 'test1', 'piped readable steam test1')
+            t.equal(actualSteams[1], 'test2', 'piped readable steam test2')
+        })
+
+        writableStream.on('finish', () => {
+            t.fail('piped readable steam finish event is not called')
+        })
+
+        writableStream.on('close', () => {
+            t.equal(readable.readableSteam.readable, true, 'piped readable steam is no readable on close event')
+            t.equal(writableStream.destroyed, true, 'piped readable steam is destroyed on close event')
+        })
+
+        writableStream.on('unpipe', () => {
+            t.true(true, 'piped readable steam unpipe event is not called')
+        })
+
+        return writableStream
     })
-    t.end()
 })

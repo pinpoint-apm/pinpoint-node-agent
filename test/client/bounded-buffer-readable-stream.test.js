dcd6d03df6118801e455e43c443bbfb9c0db66fe
Yongseok <feelform@gmail.com>
2024-03-26 20:09:03 +0900

    * writableFactory for reconnect gRPC writableSteam

--- test/client/bounded-buffer-readable-stream.test.js
+++ test/client/bounded-buffer-readable-stream.test.js
@@ -16,7 +16,7 @@ test('no piped readable steam', (t) => {
     readable.push('test2')
     readable.push(null)
 
-    t.equal(readable.steam.readable, true, 'no writable stream piped readable steam not started')
+    t.equal(readable.readableSteam.readable, true, 'no writable stream piped readable steam not started')
     t.equal(readable.buffer.length, 3, 'no writable stream piped readable steam buffer is not empty')
     t.end()
 })
@@ -27,23 +27,42 @@ test('piped readable steam', (t) => {
     readable.push('test2')
     readable.push(null)
 
+    readable.pipe(() => {
+        const actualSteams = []
+        const writable = new Writable({
+            write(chunk, encoding, callback) {
+                actualSteams.push(chunk.toString())
+                callback()
+            }
+        })
+        writable.on('finish', () => {
+            t.equal(readable.readableSteam.readable, false, 'piped readable steam finished')
+            t.equal(readable.buffer.length, 0, 'piped readable steam buffer is empty')
+
+            t.equal(actualSteams.length, 2, 'piped readable steam')
+            t.equal(actualSteams[0], 'test1', 'piped readable steam test1')
+            t.equal(actualSteams[1], 'test2', 'piped readable steam test2')
+
+            t.end()
+        })
+        return writable
+    })
+})
+
+test('reconnect piped readable steam', (t) => {
+    const readable = new BoundedBufferReadableStream({ encoding: 'utf8' })
+    readable.push('test1')
+    readable.push('test2')
+    readable.push(null)
+
     const actualSteams = []
-    const writable = new Writable({
-        write(chunk, encoding, callback) {
-            actualSteams.push(chunk.toString())
-            callback()
-        }
+    readable.pipe(() => {
+        return new Writable({
+            write(chunk, encoding, callback) {
+                actualSteams.push(chunk.toString())
+                callback()
+            }
+        })
     })
-    writable.on('finish', () => {
-        t.equal(readable.steam.readable, false, 'piped readable steam finished')
-        t.equal(readable.buffer.length, 0, 'piped readable steam buffer is empty')
-
-        t.equal(actualSteams.length, 2, 'piped readable steam')
-        t.equal(actualSteams[0], 'test1', 'piped readable steam test1')
-        t.equal(actualSteams[1], 'test2', 'piped readable steam test2')
-    
-        t.end()
-    })
-
-    readable.pipe(writable)
+    t.end()
 })

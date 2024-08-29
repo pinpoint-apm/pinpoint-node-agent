9d6504ae6d49921c480c37cde8970b3f52e67b26
Yongseok <feelform@gmail.com>
2024-08-29 14:40:36 +0900

    [#214] profilerService.handleCommandV2
    * Add gRPC fixture
    * Save AgentInfo in Agent initialization time
    * Replace MockGrpcDataSender to fixture
    
    
    [#218] The serviceType and applicationServiceType of Span are same. It doesn't needs to pass to construct parameter
    [#214]

---
+++ test/client/grpc-fixture.js
@@ -0,0 +1,111 @@
+/**
+ * Pinpoint Node.js Agent
+ * Copyright 2020-present NAVER Corp.
+ * Apache License v2.0
+ */
+
+'use strict'
+
+const config = require('../../lib/config')
+const AgentInfo = require('../../lib/data/dto/agent-info')
+const GrpcDataSender = require('../../lib/client/grpc-data-sender')
+
+let callCount = 0
+let afterCount = 0
+let callRequests = []
+let callMetadata = []
+
+function beforeSpecificOne(port, one, serviceConfig) {
+    callCount = 0
+    afterCount = 0
+    config.clear()
+    callRequests = []
+    callMetadata = []
+    const actualConfig = config.getConfig({ 'grpc.service_config': serviceConfig })
+    actualConfig.collectorIp = 'localhost'
+    actualConfig.collectorTcpPort = port
+    actualConfig.collectorStatPort = port
+    actualConfig.collectorSpanPort = port
+    actualConfig.enabledDataSending = true
+    return new one(
+        actualConfig.collectorIp,
+        actualConfig.collectorTcpPort,
+        actualConfig.collectorStatPort,
+        actualConfig.collectorSpanPort,
+        agentInfo(),
+        actualConfig
+    )
+}
+
+function agentInfo() {
+    return Object.assign(new AgentInfo({
+        agentId: '12121212',
+        applicationName: 'applicationName',
+        agentStartTime: Date.now()
+    }), {
+        ip: '1'
+    })
+}
+
+function afterOne(t) {
+    afterCount++
+    if (callCount === afterCount) {
+        t.end()
+    }
+}
+
+function getCallRequests() {
+    return callRequests
+}
+
+function getMetadata() {
+    return callMetadata
+}
+
+function increaseCallCount() {
+    callCount++
+}
+
+class DataSourceCallCountable extends GrpcDataSender {
+    constructor(collectorIp, collectorTcpPort, collectorStatPort, collectorSpanPort, agentInfo, config) {
+        super(collectorIp, collectorTcpPort, collectorStatPort, collectorSpanPort, agentInfo, config)
+    }
+
+    sendAgentInfo(agentInfo, callArguments) {
+        increaseCallCount()
+        super.sendAgentInfo(agentInfo, callArguments)
+    }
+
+    sendApiMetaInfo(apiMetaInfo, callArguments) {
+        increaseCallCount()
+        super.sendApiMetaInfo(apiMetaInfo, callArguments)
+    }
+
+    sendStringMetaInfo(stringMetaInfo, callArguments) {
+        increaseCallCount()
+        super.sendStringMetaInfo(stringMetaInfo, callArguments)
+    }
+
+    sendSqlMetaInfo(sqlMetaData, callback) {
+        increaseCallCount()
+        super.sendSqlMetaInfo(sqlMetaData, callback)
+    }
+
+    sendSqlUidMetaData(sqlMetaData, callback) {
+        increaseCallCount()
+        super.sendSqlUidMetaData(sqlMetaData, callback)
+    }
+
+    sendSpan(span) {
+        increaseCallCount()
+        super.sendSpan(span)
+    }
+}
+
+module.exports = {
+    beforeSpecificOne,
+    afterOne,
+    getCallRequests,
+    getMetadata,
+    DataSourceCallCountable,
+}

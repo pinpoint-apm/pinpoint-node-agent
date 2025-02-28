732ed042c2baf9c6651357b2fbc36de9c8205f00
Yongseok <feelform@gmail.com>
2025-02-28 21:07:58 +0900

    [#281] The deadline retry connection
    * If the Deadline and Stream occurs, Functional Tests for The Stream Must be closed
    * Apply Deadline retry connection to Stats Stream and Span Stream

--- test/client/grpc-stream-deadline.test.js
+++ test/client/grpc-stream-deadline.test.js
@@ -53,7 +53,6 @@ function createStatCall(t) {
     return statClient.sendAgentStat({ deadline: deadline }, (err, response) => {
         if (err) {
             log.error(`statStream callback err: ${err} in statClient.sendAgentStat callback`)
-            // t.equal(err.code, grpc.status.DEADLINE_EXCEEDED, `error code grpc.status.DEADLINE_EXCEEDED in statClient.sendAgentStat callback`)
         }
 
         if (response) {
---
+++ test/client/ping-grpc-readable-stream.test.js
@@ -0,0 +1,14 @@
+/**
+ * Pinpoint Node.js Agent
+ * Copyright 2021-present NAVER Corp.
+ * Apache License v2.0
+ */
+
+'use strict'
+
+const test = require('tape')
+const grpc = require('@grpc/grpc-js')
+
+test('grpc client ping server failure error retry connection test', (t) => {
+    t.end()
+})

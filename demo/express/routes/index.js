c9a9f85f8c3fff5dbb29c93d90f9b453dc554277
Yongseok <feelform@gmail.com>
2025-06-27 18:43:14 +0900

    [#8530] PostgreSQL Positional Parameters

--- demo/express/routes/index.js
+++ demo/express/routes/index.js
@@ -1,6 +1,37 @@
 var express = require('express')
 var router = express.Router()
 const axios = require('axios')
+
+const path = require('path')
+const { PostgreSqlContainer } = require('@testcontainers/postgresql')
+const { Client } = require('pg')
+
+let client
+async function startPostgreSqlContainer() {
+  const fixtures = path.resolve(__dirname, '..', 'fixtures', 'db')
+  const source = path.resolve(fixtures, 'postgresql.sql')
+  const container = await new PostgreSqlContainer('postgres:13')
+    .withDatabase('test')
+    .withUsername('testuser')
+    .withPassword('testpass')
+    .withEnvironment({
+      'TZ': 'Asia/Seoul',
+    })
+    .withCopyFilesToContainer([{
+      source: source,
+      target: '/docker-entrypoint-initdb.d/postgresql.sql'
+    }])
+    .start()
+  client = new Client({
+    host: container.getHost(),
+    port: container.getPort(),
+    database: 'test',
+    user: 'testuser',
+    password: 'testpass'
+  })
+  client.connect()
+}
+startPostgreSqlContainer()
 // const localStorage = require('pinpoint-node-agent/lib/instrumentation/context/local-storage')
 
 // const IORedis = require('ioredis')
@@ -19,7 +50,7 @@ const pool = mysql.createPool({
 })
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
   // ioRedis.set("keyio", "value", function(error) {
   //   // console.log(`ioredis set `)
   // })
@@ -35,6 +66,9 @@ router.get('/', async function(req, res, next) {
   //   port: 33066,
   //   acquireTimeout: 1000000,
   // })
+  client.query('SELECT * FROM member WHERE id = $1', ['a'], function (error, results) {
+    if (error) throw error
+  })
 
   // connection.connect()
   // const trace1 = localStorage.getStore()
@@ -65,11 +99,11 @@ router.get('/', async function(req, res, next) {
   res.render('index', { title: 'Express' })
 })
 
-router.get('/api', function(req, res, next) {
+router.get('/api', function (req, res, next) {
   res.status(200).json({ "result": "ok" })
 })
 
-router.get('/api2', function(req, res, next) {
+router.get('/api2', function (req, res, next) {
   res.status(200).json({ "result": "ok" })
 })
 

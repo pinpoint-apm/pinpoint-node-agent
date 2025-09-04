d7c14993404111b216009ef436ebb0a26b5253c3
Yongseok <feelform@gmail.com>
2025-09-04 20:25:29 +0900

    [#367] Use internal LogLevel constants

--- lib/utils/log/types.d.ts
+++ lib/utils/log/types.d.ts
@@ -4,17 +4,20 @@
  * Apache License v2.0
  */
 
-export declare enum LogLevel {
-    TRACE = 0,
-    DEBUG = 1,
-    INFO = 2,
-    WARN = 3,
-    ERROR = 4,
-    SILENT = 5
+export declare class LogLevel {
+    static readonly TRACE: 0;
+    static readonly DEBUG: 1;
+    static readonly INFO: 2;
+    static readonly WARN: 3;
+    static readonly ERROR: 4;
+    static readonly SILENT: 5;
 }
 
+export declare type LogLevelNumbers = 0 | 1 | 2 | 3 | 4 | 5;
+
 export declare interface Appender {
-    loggingLevel: LogLevel | number;
+    name: string;
+    loggingLevel: LogLevelNumbers | number;
     debug(message: string): void;
     info(message: string): void;
     warn(message: string): void;
@@ -38,14 +41,25 @@ export declare class LogBuilder {
 }
 
 export declare class Log {
-    constructor(name: string, level: LogLevel | number);
+    constructor(name: string, level: LogLevelNumbers | number);
 
     getName(): string;
-    getLevel(): LogLevel | number;
+    getLevel(): LogLevelNumbers | number;
     getAppenders(): Array<Appender>;
 }
 
-export declare class Logger {
+export declare interface Logging {
+    debug(...args: any[]): void;
+    info(...args: any[]): void;
+    warn(...args: any[]): void;
+    error(...args: any[]): void;
+
+    isDebug(): boolean;
+    isInfo(): boolean;
+}
+
+export declare class Logger implements Logging {
+
     debug(...args: any[]): void;
     info(...args: any[]): void;
     warn(...args: any[]): void;
@@ -56,3 +70,18 @@ export declare class Logger {
 
     getLogger(log: Log): Logger;
 }
+
+export declare class ChildLogger implements Logging {
+    name: string;
+
+    debug(...args: any[]): void;
+    info(...args: any[]): void;
+    warn(...args: any[]): void;
+    error(...args: any[]): void;
+
+    isDebug(): boolean;
+    isInfo(): boolean;
+}
+
+export declare const logger: Logger;
+

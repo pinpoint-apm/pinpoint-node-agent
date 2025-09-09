b4497ec97b468722f136684160d7e674bd15dbf8
Yongseok <feelform@gmail.com>
2025-09-09 11:34:39 +0900

    [#369] Extract logger.setRootLogger out of AgentBuilder

--- index.js
+++ index.js
@@ -9,8 +9,12 @@
 const { AgentBuilder } = require('./lib/agent-builder')
 const AgentInfo = require('./lib/data/dto/agent-info')
 const { getConfig } = require('./lib/config')
+const { LogBuilder } = require('./lib/utils/log/log-builder')
+const logger = require('./lib/utils/log/logger')
 
 const config = getConfig()
+logger.setRootLogger(LogBuilder.createDefaultLogBuilder().setConfig(config).build())
+
 const agentInfo = AgentInfo.make(config)
 const agent = new AgentBuilder(agentInfo)
                 .setConfig(config)

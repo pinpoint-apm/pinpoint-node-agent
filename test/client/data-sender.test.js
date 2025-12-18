8e72c3512401fa30eb3edbc5b24a29363e6430de
Yongseok <feelform@gmail.com>
2025-12-18 16:56:16 +0900

    [#404] Rename refactoring this.config.enabledStatsMonitor to this.config.isStatsMonitoringEnabled()
    [#404] Rename refactoring this.config.enabledStatsMonitor to this.config.isStatsMonitoringEnabled()

--- test/client/data-sender.test.js
+++ test/client/data-sender.test.js
@@ -7,10 +7,7 @@
 const test = require('tape')
 
 const agent = require('../support/agent-singleton-mock')
-const dataSenderMock = require('../support/data-sender-mock')
-const dataSender = dataSenderMock()
 const AgentInfo = require('../../lib/data/dto/agent-info')
-const agentInfo = AgentInfo.make(agent.config)
 const ApiMetaInfo = require('../../lib/data/dto/api-meta-info')
 const StringMetaInfo = require('../../lib/data/dto/string-meta-info')
 const defaultPredefinedMethodDescriptorRegistry = require('../../lib/constant/default-predefined-method-descriptor-registry')
@@ -19,7 +16,8 @@ test('Should send agent info', function (t) {
   t.plan(1)
   agent.bindHttp()
 
-  dataSender.send(agentInfo)
+  const agentInfo = AgentInfo.make(agent.config)
+  agent.dataSender.send(agentInfo)
 
   t.ok(true)
 })
@@ -30,9 +28,9 @@ test('Should send api meta info', function (t) {
 
   const methodDescriptor = defaultPredefinedMethodDescriptorRegistry.nodeServerMethodDescriptor
   const apiMetaInfo = ApiMetaInfo.create(methodDescriptor)
-  dataSender.send(apiMetaInfo)
+  agent.dataSender.send(apiMetaInfo)
 
-  t.equal(dataSender.mockAPIMetaInfos[0], apiMetaInfo, "span is equal in datasender")
+  t.equal(agent.dataSender.mockAPIMetaInfos[0], apiMetaInfo, "span is equal in datasender")
 })
 
 test('Should send string meta info', function (t) {
@@ -40,7 +38,7 @@ test('Should send string meta info', function (t) {
   agent.bindHttp()
 
   const stringMetaInfo = StringMetaInfo.create('1', 'test string')
-  dataSender.send(stringMetaInfo)
+  agent.dataSender.send(stringMetaInfo)
 
-  t.deepEqual(dataSender.mockMetaInfo, stringMetaInfo, "agentInfo is equal in datasender")
+  t.deepEqual(agent.dataSender.mockMetaInfo, stringMetaInfo, "agentInfo is equal in datasender")
 })
